3503e38191bd86afc5b4f4d85a1d977da9a2c613
Valentina Troiano <valetroiano.vt@gmail.com>
2022-03-31 12:42:50 -0300

    desafio 2

---
+++ manejoArchivos.js
@@ -0,0 +1,41 @@
+const fs = require ('fs')
+
+const arr = []
+
+let ultimoId = 0
+
+class Caja {
+    constructor(ruta){
+        this.ruta = ruta
+        this.cosas = []
+    }
+
+    async guardar (cosa){
+        cosa.id = ultimoId +1 
+        this.cosas.push (cosa)
+
+        try{
+            await fs.promises.writeFile(this.ruta, JSON.stringify(this.cosas))
+        console.log(cosas)
+        console.log("guardado")
+
+        } catch (err){
+            console.log("error")
+        }
+     
+
+        return ultimoId
+
+    }
+    
+}
+
+const caja =new Caja()
+
+const cosa = [
+    { nombre:"escuadra", precio: 30, thumbnail: "http://http2.mlstatic.com/D_656522-MLA44388481439_122020-I.jpg"},
+    { nombre: "globo terraqueo", precio: 50, thumbnail: "http://http2.mlstatic.com/D_920961-MLA31579848144_072019-I.jpg"},
+    { nombre :"calculadora", precio:40, thumbnail: "http://http2.mlstatic.com/D_875724-MLA31116238699_062019-O.jpg" }
+]
+
+caja.guardar(cosa)
